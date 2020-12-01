b2624781fb3ebc501da9d597ab3d4603b1bf7c9e
zhaoyang <zhaoyang01@ebnew.com>
2020-12-01 13:57:03 +0800

    添加 加一算法

---
+++ src/main/java/org/example/week01/n66_加一.java
@@ -0,0 +1,79 @@
+package org.example.week01;
+
+//给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+//
+// 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+//
+// 你可以假设除了整数 0 之外，这个整数不会以零开头。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：digits = [1,2,3]
+//输出：[1,2,4]
+//解释：输入数组表示数字 123。
+//
+//
+// 示例 2：
+//
+//
+//输入：digits = [4,3,2,1]
+//输出：[4,3,2,2]
+//解释：输入数组表示数字 4321。
+//
+//
+// 示例 3：
+//
+//
+//输入：digits = [0]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+//
+// Related Topics 数组
+// 👍 589 👎 0
+
+
+import java.util.Arrays;
+
+public class n66_加一 {
+
+
+    public static void main(String[] args) {
+        int [] a = {9,9,9,9,9};
+
+        int [] b = {1,2,3,4,5};
+
+        System.out.println(Arrays.toString(plusOne(a)));
+        System.out.println(Arrays.toString(plusOne(b)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        if(digits[0] == 0){
+            int[] array = new int[digits.length +1];
+            array[0] = 1;
+            return array;
+        }
+
+        return digits;
+
+    }
+}
